5bb9cbc814c5028ab4f89ba55a913ff2932d53ef
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-20 15:13:00 +0900

    React-fontawesome icons added

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Work from "./components/Work";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Header from './components/Header';
-import Footer from "./components/Footer";
+import Work from "./components/Home/Work";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Header from './components/Header/Header';
+import Footer from "./components/Footer/Footer";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faDribbble } from '@fortawesome/free-brands-svg-icons';
+
+
+library.add(faEnvelope, faLinkedin, faGithub, faDribbble); 
 
 
 function App() {
