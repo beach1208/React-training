1a0cebb975b49ca92e6f7282a18e83acc159738b
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-29 17:07:30 +0900

    Rewrote class component

--- react-is-fun/src/Library.jsx
+++ react-is-fun/src/Library.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import Book from "./Book"
 
-const Library = ({books}) => {
-    return(
-       <div>
-           {books.map(book => <Book key={book.key}  title={book.title} author={book.author} pages={book.pages}/>
+
+class Library extends React.Component {
+    state = { 
+        open: false,
+        freeBookmark: false
+    } 
+
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open,
+        }))
+    }
+    render(){
+        const { books } = this.props;
+        return(
+            <div>
+                <h1>The librry is {this.state.open? 'open' : 'closed'}</h1>
+                <button onClick={this.props.toggleOpenClosed}>Change</button>
+                {books.map(book => 
+                <Book
+                    key={book.key}
+                    title={book.title}
+                    author={book.author}
+                    pages={book.pages}
+                    freeBookmark={this.state.freeBookmark}
+                />
            )}
-       </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Library;
