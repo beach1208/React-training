18e93d6faef64e0cd4483dca6efef7ce51e71472
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-30 12:40:23 +0900

    Added conditional redered elements

--- react-is-fun/src/Library.jsx
+++ react-is-fun/src/Library.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Book from "./Book"
 
+const Hiring = () => 
+    <div>
+        <p>The library is hiring.</p>
+    </div>
+
+const NotHiring = () => 
+    <div>
+        <p>The library is not hiring.</p>
+    </div>
 
 class Library extends React.Component {
     state = { 
         open: false,
-        freeBookmark: false
+        freeBookmark: false,
+        hiring: true
     } 
 
     toggleOpenClosed = () => {
@@ -17,8 +27,9 @@ class Library extends React.Component {
         const { books } = this.props;
         return(
             <div>
-                <h1>The librry is {this.state.open? 'open' : 'closed'}</h1>
-                <button onClick={this.props.toggleOpenClosed}>Change</button>
+                {this.state.hiring ? <Hiring/> : <NotHiring/>}
+                <h1>The librry is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(book => 
                 <Book
                     key={book.key}
