7d0020ee67dccaaf58ae4901b66f3fc8e82f3d54
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-02-02 15:44:07 +0900

    Added reducers

---
+++ Ex_Files_Learning_Redux/Ch02/02_03/start/src/store/reducers.js
@@ -0,0 +1,18 @@
+import C from '../constants'
+
+export const goal = (state=10, action) => {
+
+   if (action.type === C.SET_GOAL) {
+   	   return parseInt(action.payload)
+   } else {
+       return state
+   }
+}
+
+export const SkiDay = (state=null, action) => {
+    if(action.type === C.ADD_DAY) {
+        return action.payload
+    }else {
+        return state
+    }
+}
